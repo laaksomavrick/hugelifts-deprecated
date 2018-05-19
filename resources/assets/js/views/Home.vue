f11dfaf2e6a9f9b455f3377523934ae370241d97
mlaakso@dev <laakso.mavrick@gmail.com>
2018-05-19 16:52:20 -0400

    experimenting, gitignore

--- resources/assets/js/views/Home.vue
+++ resources/assets/js/views/Home.vue
@@ -1,3 +1,26 @@
 <template>
     <p>homepage</p>
 </template>
+
+<script>
+
+export default {
+
+    mounted: async function() {
+
+      const test = () => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log("ping!")
+                resolve()
+            }, 5000)
+        })
+      }
+
+      await test()
+
+    }
+
+}
+
+</script>
