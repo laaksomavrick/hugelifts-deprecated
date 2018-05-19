4681b6aa12d54e25b918cec453a48f215913987e
mlaakso@dev <laakso.mavrick@gmail.com>
2018-05-19 16:32:27 -0400

    initial commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import App from './views/App'
+import Home from './views/Home'
+import About from './views/About'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
+        }
+    ]
+})
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router
+})
