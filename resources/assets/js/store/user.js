b996ea0ec55c5c94c529f8dbc68f580413e3fdee
mlaakso@dev <laakso.mavrick@gmail.com>
2018-05-26 12:22:14 -0400

    bootstrap, vuex user store

---
+++ resources/assets/js/store/user.js
@@ -0,0 +1,26 @@
+const state = {
+    user: {},
+    authenticated: false
+}
+
+const actions = {
+
+}
+
+const mutations = {
+
+}
+
+const getters = {
+
+  getUser: state => state.user,
+  getAuthenticated: state => state.authenticated
+
+}
+
+export default {
+    state,
+    actions,
+    mutations,
+    getters
+}
