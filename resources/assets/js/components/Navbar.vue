bd7bf74eedbf3eba07a12d61fb504f1a670ba7bb
mlaakso@dev <laakso.mavrick@gmail.com>
2018-07-05 22:31:15 -0400

    navbar css

--- resources/assets/js/components/Navbar.vue
+++ resources/assets/js/components/Navbar.vue
@@ -1,29 +1,46 @@
 <template>
-    <nav class="navbar">
-        <div class="navbar__brand">
+    <b-navbar 
+        toggleable
+        type="dark"
+        variant="primary">
+
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+        <b-navbar-brand class="navbar__brand">
             <font-awesome-icon 
                 class="navbar__brand-icon" 
                 :icon="brandIcon" 
             />
-        </div>
-        <div class="navbar__routes">
-            <template v-for="route in routes">
-                <div class="navbar__item" v-bind:class="{ 'navbar__item--active': isActive(route.route) }">
-                    <router-link :to="{ name: route.route}" class="navbar__link">{{ route.text }}</router-link>
+        </b-navbar-brand>
+
+        <b-collapse is-nav id="nav_collapse">
+            <b-navbar-nav>
+                <template v-for="(route, index) in routes">
+                    <b-nav-item 
+                        class="navbar__item" 
+                        v-bind:key="index"
+                        v-bind:class="{ 'navbar__item--active': isActive(route.route) }"
+                        :to="{ name: route.route}">
+                        {{ route.text }}
+                    </b-nav-item>
+                </template>
+            </b-navbar-nav>
+
+            <b-navbar-nav class="ml-auto">
+                <div class="navbar__actions">
+                    <div class="navbar__action">
+                        <font-awesome-icon 
+                            class="navbar__plus-icon" 
+                            :icon="plusIcon" 
+                            v-if="addIsVisible"
+                            @click="handleAddClick" 
+                        />
+                    </div>
                 </div>
-            </template>
-        </div>
-        <div class="navbar__actions">
-            <div class="navbar__action">
-                <font-awesome-icon 
-                    class="navbar__plus-icon" 
-                    :icon="plusIcon" 
-                    v-if="addIsVisible"
-                    @click="handleAddClick" 
-                />
-            </div>
-        </div>
-    </nav>
+            </b-navbar-nav>
+
+        </b-collapse>
+    </b-navbar>
 </template>
 
 <script>
@@ -96,4 +113,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.navbar {
+
+    color: white;
+    margin-bottom: 1rem;
+    box-shadow: 
+        0 3px 1px -2px rgba(0,0,0,.2), 
+        0 2px 2px 0 rgba(0,0,0,.14), 
+        0 1px 5px 0 rgba(0,0,0,.12);
+
+    &__item {
+        color: white;
+    }
+
+}
+
 </style>
